01f0eb9cb275dee6fbc182afeb943d45d2a8f108
Paulo Cardoso <paulotomas14@gmail.com>
2019-04-06 22:52:16 +0100

    cfrvgbhnj

--- ShiftAppens/Public/code/index.js
+++ ShiftAppens/Public/code/index.js
@@ -1,4 +1,10 @@
 "use strict";
+const host = 'https://keystrokestestesecenas.netlify.com/';
+	// Port we are connecting on
+const port = 1337;
+
+const socket = new jSocket();
+
 
 (function()
 {
@@ -10,29 +16,26 @@ function main(){
 	ListenForData();
 };
 
-void ListenForData()
-{
-    try
-    {
-        socketConnection = new TcpClient("https://keystrokestestesecenas.netlify.com/", 1337);
-        var bytes = new Byte[1024];
-        while (true)
-        {
 
-            using (StreamReader sr = new StreamReader(socketConnection.GetStream()))
-            {
-                var string line;
-                // Read and display lines from the file until the end of
-                // the file is reached.
-                while ((line = sr.ReadLine()) != null)
-                {
-                    console.log(line);
-                }
-            }
-        }
-    }
-    catch (SocketException socketException)
-    {
-        Debug.Log("Socket exception: " + socketException);
-    }
+ListenForData(){
+	// When the socket is added the to document
+	socket.onReady = function(){
+					socket.connect(host, port);
+	}
+
+	// Connection attempt finished
+	socket.onConnect = function(success, msg){
+					if(success){
+									// Send something to the socket
+									socket.write('Hello world');            
+					}else{
+									alert('Connection to the server could not be estabilished: ' + msg);
+					}
+	}
+	socket.onData = function(data){
+					alert('Received from socket: '+data);
+	}
+
+	// Setup our socket in the div with the id="socket"
+	socket.setup('mySocket');
 }
