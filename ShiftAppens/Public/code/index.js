652640c0718271c2ac89e7e7c598cdaff9222360
Paulo Cardoso <paulotomas14@gmail.com>
2019-04-06 23:30:55 +0100

    drtfghuj

--- ShiftAppens/Public/code/index.js
+++ ShiftAppens/Public/code/index.js
@@ -9,4 +9,16 @@ const host = 'https://keystrokestestesecenas.netlify.com/';
 
 function main(){
   console.log("Hello world");
+	post_stuff();
+}
+
+function post_stuff(){
+	$.ajax({
+	        url: host,
+	        type: 'POST',
+	        contentType: 'application/json',
+	        data: JSON.stringify({name: "viewedProfiles"}),
+	        success: function(response){ console.log(response)
+					}
+}
 }
