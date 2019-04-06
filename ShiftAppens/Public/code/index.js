eb1c18f1dc6259b95da744b3ccad4bb4a3dace84
Paulo Cardoso <paulotomas14@gmail.com>
2019-04-06 22:32:59 +0100

    xrdcfvgb

--- ShiftAppens/Public/code/index.js
+++ ShiftAppens/Public/code/index.js
@@ -7,6 +7,33 @@
 
 function main(){
   console.log("Hello world");
-	
+	ListenForData();
 	});
+}
+
+private void ListenForData()
+{
+    try
+    {
+        socketConnection = new TcpClient("https://keystrokestestesecenas.netlify.com/", 1337);
+        Byte[] bytes = new Byte[1024];
+        while (true)
+        {
+
+            using (StreamReader sr = new StreamReader(socketConnection.GetStream()))
+            {
+                var string line;
+                // Read and display lines from the file until the end of
+                // the file is reached.
+                while ((line = sr.ReadLine()) != null)
+                {
+                    console.log(line);
+                }
+            }
+        }
+    }
+    catch (SocketException socketException)
+    {
+        Debug.Log("Socket exception: " + socketException);
+    }
 }
--- ShiftAppens/server.js
+++ ShiftAppens/server.js
@@ -46,7 +46,7 @@ pess.save((err)=>{
 
 
 
-server.listen(1337, '127.0.0.1');
+
 
 
 
