8053e94b9f55c5172b111f5b3f52df8b0929be62
Paulo Cardoso <paulotomas14@gmail.com>
2019-04-07 03:51:45 +0100

    ctrfvygbhunj

--- ShiftAppens/Public/code/index.js
+++ ShiftAppens/Public/code/index.js
@@ -24,7 +24,7 @@ function funcao_teste(e){
 
 	$.ajax(
 	{
-					url: "/",
+					url: "(index)",
 					type: 'POST',
 					contentType: 'application/json',
 					'X-Requested-With': 'XMLHttpRequest',
